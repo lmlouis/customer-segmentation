068c4c76183c12f26e2a11b2139250783d7dfd08
lmlouis <louiswora1@gmail.com>
2022-12-15 16:11:03 +0100

    deploiement github

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import pickle
-import sklearn 
+
 from prediction_function.module import ciblage, chiffre_affaire,collect_parametres
 
 with open("css/style.css") as f:
