05754b6163df8d21c7c7de165f5643fd650eedaf
WORA SOUAMY Louis Martin <80777092+lmlouis@users.noreply.github.com>
2023-12-04 06:29:45 +0100

    Update app.py
    appply ui

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
+import os
+import utils as utl
+from PIL import Image
+import time
 import streamlit as st
 import pandas as pd
 import pickle
 from prediction_function.module import ciblage, chiffre_affaire,collect_parametres
 
-with open("css/style.css") as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+def main():    
+    # Settings
+    st.set_page_config(layout="wide", page_title='Demo item')
+    utl.set_page_title('Costumer Segmentation')
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    # Loading CSS
+    utl.local_css("css/style.css")
+    utl.remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+    # Logo
+    dir_root = os.path.dirname(os.path.abspath(__file__))
+    logo = Image.open(dir_root+'/logo-lm.png')
+    # Selecting a job
+    # st.sidebar.image(logo)
+    st.sidebar.selectbox('Select',('loren','Ipsum'))
+    st.sidebar.multiselect('Multi',('loren','Ipsum'))
+    st.sidebar.date_input('Date')
+    st.sidebar.text_input('Text')
+    st.sidebar.slider('Slider',min_value=5,max_value=20)
+    st.warning('Warning')
+    st.info('Info')
+    st.error('Error')
+    
+    with utl.stNotification('Sample notification, always on top and floats (spiner is optional)'):
+        time.sleep(5)
 
 
-IMAGE_URL = "background.jpg"
-
-st.image(IMAGE_URL)
+if __name__ == '__main__':
+    main()
 
 st.write(
     """
